e22bfaa4c1440ceb0f631fd157c8f8b9f9e4b0e8
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-10-28 21:42:45 +0700

    Refactor code and update data rendering logic

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,25 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
-console.log('Hello Coders! :)');
+import data from '../public/data/DATA.json';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const restaurantList = document.getElementById('restaurant-list');
+
+    data.restaurants.forEach((restaurant) => {
+        const restaurantItem = document.createElement('div');
+        restaurantItem.classList.add('restaurant-item');
+
+        restaurantItem.innerHTML = `
+            <img src="${restaurant.pictureId}" alt="${restaurant.name}">
+            <div class="content">
+                <p>Kota: ${restaurant.city}</p>
+                <p>Rating: ${restaurant.rating}</p>
+                <h3>${restaurant.name}</h3>
+                <p>${restaurant.description.substring(0, 100)}...</p>
+            </div>
+        `;
+
+        restaurantList.appendChild(restaurantItem);
+    });
+});
